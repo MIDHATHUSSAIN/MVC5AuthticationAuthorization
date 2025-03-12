57c6e769a530371eba8acd2c4026c0893e1d3b94
MIDHAT-HUSSAIN <135623033+MIDHATHUSSAIN@users.noreply.github.com>
2025-03-12 01:35:12 -0700

    Add files via upload

---
+++ WebApiConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using WebApiThrottle;
+
+namespace webAPIandMVC
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MessageHandlers.Add(new ThrottlingHandler()
+            {
+                Policy = new ThrottlePolicy(perMinute: 5) // 5 requests per minute
+                {
+                    IpThrottling = true
+                },
+                Repository = new MemoryCacheRepository()
+            });
+
+          config.MapHttpAttributeRoutes();
+        
+            //config.MapHttpAttributeRoutes();
+
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //    config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{action}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+        }
+     
+    }
+}
